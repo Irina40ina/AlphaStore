a37969f7f37db3f41b0ad538875224ef718080d3
Irina40ina <kanishevairina@mail.ru>
2024-09-10 18:36:30 +0300

    part 2

---
+++ src/components/filter/aptAreaComp.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="d-flex flex-md-column w-100 px-2">
+        <h4 class="apt-area-title">Общая площадь м<sup>2</sup></h4>
+        <v-range-slider 
+        density="compact" 
+        thumb-color="var(--thumb-color)" 
+        track-color="var(--track-color)" 
+        track-fill-color="var(--track-fill-color)" 
+        thumb-size="15"
+        step="10"
+        thumb-label="true"
+        strict="false"
+        tick-size="2"
+        ></v-range-slider>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.apt-area-title {
+    color: var(--fg-color);
+    font-family: var(--basic-font);
+    margin-bottom: .8rem;
+}
+</style>
---
+++ src/components/filter/filterComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="main-filter-container">
+        <h2 class="filter-title">Фильтры</h2>
+        <aptAreaComp></aptAreaComp>
+    </div>
+</template>
+<script setup>
+import aptAreaComp from './aptAreaComp.vue';
+</script>
+<style scoped>
+.main-filter-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.filter-title {
+    text-align: center;
+    color: var(--basic-colorful-fg);
+    font-family: var(--basic-font);
+}
+</style>
